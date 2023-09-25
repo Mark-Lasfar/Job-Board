165244cfff82e9e6d1d213c75ed7bd8bc39f474f
mark-lasfar <marklasfar@gmail.com>
2023-09-25 07:40:16 -0400

    add Job models

--- job/models.py
+++ job/models.py
@@ -35,3 +35,7 @@ class Job(models.Model): #table
     Vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+
+
+    def __str__(self):
+        return self.title
